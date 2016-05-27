ca7b6a3bc3aad4a7634c2c0378ab844a2f4cfd4e
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-05-27 02:05:45 +0200

    Updated directory structure

---
+++ challenges/php/001/001-challenge.php
@@ -0,0 +1,7 @@
+<?php
+    $arr=[1,2,'3','a'];
+    $str="Hello World";  
+    function doSomeMaths($a, $b){
+      return $a + $b;
+    }
+?>
---
+++ challenges/php/001/001-test.php
@@ -0,0 +1,21 @@
+<?php
+   // Array:
+   // -------
+   // Length:
+   assert('count($arr)===3', 'The array is not the good size. It must contain 3 elements');
+   // Types:
+
+   for($i=0; $i<count($arr); $i++){
+    assert( '(int)$arr[$i] === $arr[$i]', 'Element '.($i+1).' is not an integer');
+   }
+
+   // String:
+   // -------
+   // Value:
+   assert('$str==="Hello world"', 'Check your string, something is wrong.');
+
+   // Function:
+   // ---------
+   // Output:
+   assert('doSomeMaths(2,2)===4 && doSomeMaths(5,10)===50', 'Your doSomeMaths function should return the product of the two arguments.');
+?>
